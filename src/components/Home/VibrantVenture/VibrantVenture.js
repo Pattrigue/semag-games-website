5a744dc8d9c483c8bdc5f5eb3e7ea1c8799b5500
Pattrigue <patrick@gamehouse.dk>
2021-08-08 21:42:06 +0200

    Finish Vibrant Venture component

--- src/components/Home/VibrantVenture/VibrantVenture.js
+++ src/components/Home/VibrantVenture/VibrantVenture.js
@@ -1,24 +1,29 @@
 import React from "react";
 import "./VibrantVenture.scss";
 import "../Home.scss";
+import {Link} from "react-router-dom";
 
 function VibrantVenture() {
     return (
         <div className="VibrantVenture main-container"
              style={{
-                 backgroundImage: `url(${process.env.PUBLIC_URL + '/vibrant-venture-logo.png'})`
+                 backgroundImage: `url(${process.env.PUBLIC_URL + '/vibrant-venture-main.png'})`
              }}>
             <div className="bg-overlay"/>
-            <div className="text-container">
-                <div className="header-text">
+            <div className="text-container vibrant-venture-container">
+                <div className="header-text vibrant-venture-header">
                     Vibrant Venture
                 </div>
                 <div>
-                    Vibrant Venture is a singleplayer 2D adventure platformer game <br/>
-                    featuring a quirky cast of characters! <br/> <br/>
-                    The game features four playable characters with multiple unique abilities <br/>
-                    that can be combined for extremely versatile, fluid and fun movement! <br/>
+                    Vibrant Venture is a singleplayer 2D adventure <br/>
+                    platformer game featuring a quirky cast of characters! <br/> <br/>
+                    The game features four playable characters with <br/>
+                    ultiple unique abilities that can be combined for <br/>
+                    extremely versatile, fluid and fun movement!
                 </div>
+                <button className="learn-more-btn">Learn More</button>
+                <span className="or">or</span>
+                <Link className="buy-now-link">Buy Now</Link>
             </div>
         </div>
     );
