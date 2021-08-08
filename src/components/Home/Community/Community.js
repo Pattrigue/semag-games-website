0b36a9092bbd22e591f8f41ba627ea90749678e2
Pattrigue <patrick@gamehouse.dk>
2021-08-08 23:57:39 +0200

    Finish community tab

--- src/components/Home/Community/Community.js
+++ src/components/Home/Community/Community.js
@@ -1,29 +1,58 @@
 import React from "react";
 import "../Home.scss";
 import "./Community.scss";
+import { Link } from 'react-router-dom';
+import { ReactComponent as DiscordIcon } from "./discord-icon.svg";
+import { ReactComponent as YouTubeIcon } from "./youtube-icon.svg";
+import { ReactComponent as TwitterIcon } from "./twitter-icon.svg";
 
 function Community() {
     return (
         <div className="Community main-container"
              style={{
-                 backgroundImage: `url(${process.env.PUBLIC_URL + '/vibrant-venture-banner.png'})`
+                 backgroundImage: `url(${process.env.PUBLIC_URL + '/vibrant-venture-community.png'})`
              }}>
             <div className="bg-overlay"/>
             <div className="text-container community-container">
                 <div className="header-text">
                     Community
                 </div>
-                <div>
+                <div className="discord-text">
                     We care a lot about our players. <br/>
                     Have any feedback, ideas or maybe you just want to talk to us?
                 </div>
-                <button>Join the Community</button>
-                <div>
+                <Link to={{ pathname: "https://discord.gg/semag-games" }}
+                      target="_blank"
+                      style={{ textDecoration: 'none' }}>
+                    <button className="btn discord-btn">
+                        <DiscordIcon className="btn-icon"/>
+                        Join the Community
+                    </button>
+                </Link>
+                <div className="youtube-twitter-text">
                     You can find devlogs and content related to Vibrant Venture and our projects on YouTube. <br/>
                     We also post important news on Twitter.
                 </div>
-                <button>Twitter</button>
-                <button>YouTube</button>
+                <div className="social-media-btns">
+                    <Link
+                        to={{ pathname: "https://www.youtube.com/c/SemagGames" }}
+                        target="_blank"
+                        style={{ textDecoration: 'none' }}>
+                        <button className="btn youtube-btn">
+                            <YouTubeIcon className="btn-icon"/>
+                            YouTube
+                        </button>
+                    </Link>
+                    <Link
+                        to={{ pathname: "https://twitter.com/pattrigue" }}
+                        target="_blank"
+                        style={{ textDecoration: 'none' }}>
+                        <button className="btn twitter-btn">
+                            <TwitterIcon className="btn-icon"/>
+                            Twitter
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
