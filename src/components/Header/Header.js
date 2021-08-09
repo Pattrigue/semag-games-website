d3c439cc8d0383d4b6ffa6706752af8a9b35ce82
RicoVDK <83656086+RicoVDK@users.noreply.github.com>
2021-08-09 14:39:45 +0200

    Attempt at variables but no dice :(

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,31 +4,31 @@ import './Header.scss'
 
 function Header() {
     return (
-        <div className="Header">
-            <div className="company-logo-container">
-                <img src={`/${process.env.PUBLIC_URL}semag-logo.png`}
-                     width="80"
-                     height="80"
-                     alt=""/>
-                <div className="company-name">
-                    SEMAG GAMES
-                </div>
-            </div>
-            <div className="nav-bar">
-                <NavLink
-                    className="nav-link"
-                    activeClassName="nav-link-active"
-                    exact to="/">Home</NavLink>
-                <NavLink
-                    className="nav-link"
-                    activeClassName="nav-link-active"
-                    to="/about">About</NavLink>
-                <NavLink
-                    className="nav-link"
-                    activeClassName="nav-link-active"
-                    to="/about">Vibrant Venture</NavLink>
-            </div>
-        </div>
+				<div className="Header">
+						<div className="company-logo-container">
+								<img src={`/${process.env.PUBLIC_URL}semag-logo.png`}
+										width="80"
+										height="80"
+										alt=""/>
+								<div className="company-name">
+										SEMAG GAMES
+								</div>
+						</div>
+						<div className="nav-bar">
+								<NavLink
+										className="nav-link"
+										activeClassName="nav-link-active"
+										exact to="/">Home</NavLink>
+								<NavLink
+										className="nav-link"
+										activeClassName="nav-link-active"
+										to="/about">About</NavLink>
+								<NavLink
+										className="nav-link"
+										activeClassName="nav-link-active"
+										to="/about">Vibrant Venture</NavLink>
+						</div>
+				</div>
     )
 }
 
