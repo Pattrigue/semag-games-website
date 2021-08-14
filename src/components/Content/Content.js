95e627deeef68375b606ff5ed9dc896c608bfd8a
Pattrigue <patrick@gamehouse.dk>
2021-08-14 22:36:15 +0200

    Update Content.js

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,10 +1,11 @@
 import React from "react";
+import {Route} from "react-router-dom";
 import Home from "../Home/Home";
 
 function Content() {
     return (
         <div className="Content">
-            <Home/>
+            <Route exact path="/" component={Home}/>
         </div>
     );
 }
