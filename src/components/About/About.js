f18aefbc5b500de18230d0610c760d46650cc9d9
Pattrigue <patrick@gamehouse.dk>
2021-08-14 22:43:02 +0200

    Setup about page

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>hi
+            <div>this is still a wip</div>
+            <div>come back later</div>
+        </div>
+    );
+}
+
+export default About;
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Route} from "react-router-dom";
 import Home from "../Home/Home";
+import About from "../About/About.js";
 
 function Content() {
     return (
         <div className="Content">
             <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
         </div>
     );
 }
