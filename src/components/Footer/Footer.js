069a603a9d804a2148964d135abc52f5c4f30fc8
RicoVDK <83656086+RicoVDK@users.noreply.github.com>
2021-11-06 00:09:22 +0100

    Added developers to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,19 @@ import React from "react"
 function Footer() {
     return (
         <div className="Footer">
-            <div className="copyright-text">© Semag Games 2021</div>
-            <img
-                src={`${process.env.PUBLIC_URL}leafly-present.png`}
-                width="80"
-                height="80"
-                alt="a"
-            />
+            <div>
+                <img
+                    src={`${process.env.PUBLIC_URL}leafly-present.png`}
+                    width="80"
+                    height="80"
+                    alt="a"
+                />
+                <div className="copyright-text">© Semag Games 2021</div>
+            </div>
+
+            <div className="developers-text">
+                Created by Pattrigue {`&`} Rico
+            </div>
         </div>
     );
 }
