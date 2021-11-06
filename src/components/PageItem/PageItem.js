21960122a566b5cf1966772e4d46abfa7b9b603d
Pattrigue <patrick@gamehouse.dk>
2021-11-06 19:53:26 +0100

    Revert "PageItem fade in"
    This reverts commit d960f3fa760e192c26ba3480ef5e076248d59e0a.

--- src/components/PageItem/PageItem.js
+++ src/components/PageItem/PageItem.js
@@ -1,34 +1,25 @@
 import React from "react";
 import BackgroundOverlay from "../BackgroundOverlay/BackgroundOverlay.js";
-import { BackgroundImage } from "react-image-and-background-image-fade";
 
 function PageItem(
     {
         children, className, align, height, backgroundImage,
         borderTop = "0", borderBottom = "0"
     }) {
-
     return (
-        <div style={{
-            minHeight: height,
-            borderTop: `${borderTop}px solid white`,
-            borderBottom: `${borderBottom}px solid white`
-        }}>
-            <BackgroundImage className={`${className} main-container relative`}
-                src={process.env.PUBLIC_URL + backgroundImage}
-                width="100%"
-                transitionTime="1s"
-                style={{
-                    minHeight: height
-                }}
-            >
+        <div className={`${className} main-container relative`}
+             style={{
+                 backgroundImage: `url(${process.env.PUBLIC_URL + backgroundImage})`,
+                 minHeight: height,
+                 borderTop: `${borderTop}px solid white`,
+                 borderBottom: `${borderBottom}px solid white`
+             }}>
             <BackgroundOverlay/>
-                <div className={`main-container-inner ${align}`}>
-                    <div className="text-container">
+            <div className={`main-container-inner ${align}`}>
+                <div className="text-container">
                     {children}
-                    </div>
                 </div>
-            </BackgroundImage>
+            </div>
         </div>
     );
 }
