d960f3fa760e192c26ba3480ef5e076248d59e0a
Pattrigue <patrick@gamehouse.dk>
2021-11-06 19:02:25 +0100

    PageItem fade in

--- src/components/PageItem/PageItem.js
+++ src/components/PageItem/PageItem.js
@@ -1,25 +1,34 @@
 import React from "react";
 import BackgroundOverlay from "../BackgroundOverlay/BackgroundOverlay.js";
+import { BackgroundImage } from "react-image-and-background-image-fade";
 
 function PageItem(
     {
         children, className, align, height, backgroundImage,
         borderTop = "0", borderBottom = "0"
     }) {
+
     return (
-        <div className={`${className} main-container relative`}
-             style={{
-                 backgroundImage: `url(${process.env.PUBLIC_URL + backgroundImage})`,
-                 minHeight: height,
-                 borderTop: `${borderTop}px solid white`,
-                 borderBottom: `${borderBottom}px solid white`
-             }}>
+        <div style={{
+            minHeight: height,
+            borderTop: `${borderTop}px solid white`,
+            borderBottom: `${borderBottom}px solid white`
+        }}>
+            <BackgroundImage className={`${className} main-container relative`}
+                src={process.env.PUBLIC_URL + backgroundImage}
+                width="100%"
+                transitionTime="1s"
+                style={{
+                    minHeight: height
+                }}
+            >
             <BackgroundOverlay/>
-            <div className={`main-container-inner ${align}`}>
-                <div className="text-container">
+                <div className={`main-container-inner ${align}`}>
+                    <div className="text-container">
                     {children}
+                    </div>
                 </div>
-            </div>
+            </BackgroundImage>
         </div>
     );
 }
