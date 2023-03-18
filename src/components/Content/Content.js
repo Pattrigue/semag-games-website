f140fe87597ef52da7e1ca84e1fc392738f4066a
Pattrigue <patrick@gamehouse.dk>
2023-03-18 20:58:03 +0100

    Update Content.js

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,14 +2,12 @@ import React from "react";
 import {Route} from "react-router-dom";
 import Home from "../Home/Home";
 import About from "../About/About.js";
-import UnderConstruction from "../UnderConstruction/UnderConstruction.js";
 
 function Content() {
     return (
         <div className="Content">
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
-            <Route path="/vibrant-venture" component={UnderConstruction}/>
         </div>
     );
 }
