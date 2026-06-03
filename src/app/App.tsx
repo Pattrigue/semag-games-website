d7a488562a3b5167e9c5282e953cd009064f406e
Pattrigue <patrick@gamehouse.dk>
2026-06-03 20:12:13 +0200

    Fix import order

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
-import { AppShell, MantineProvider, Stack } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+import { AppShell, MantineProvider } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { Header } from "@/app/Header";
 import { Navbar } from "@/app/Navbar";
 import { theme } from "@/app/theme";
 import { Home } from "@/pages/home/Home";
-import "@mantine/core/styles.css";
 
 export function App() {
   const [opened, { toggle }] = useDisclosure();
