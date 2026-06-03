fdac02e33b6413196f537e261e152adc373a01ad
Pattrigue <patrick@gamehouse.dk>
2026-06-04 00:13:35 +0200

    Remove divider

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,7 +8,6 @@ export function Home() {
   return (
     <Stack gap={0}>
       <HeroSection />
-      <Box bg="white" h={4} />
       <PromoSection />
       <ScreenshotsSection />
       <MakeItYoursSection />
