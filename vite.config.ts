ddb478fc2de857b233404ef97310bdb875c3a160
Pattrigue <patrick@gamehouse.dk>
2026-06-03 19:55:30 +0200

    Formatting

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-	resolve: {
-		alias: {
-			"@": "/src",
-		},
-	},
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
