9f61f3ba0a2b481d34f60be15c4dd62e67b2c790
Pattrigue <patrick@gamehouse.dk>
2026-06-07 11:17:55 +0200

    Remove Nunito

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,14 +1,11 @@
-import "@fontsource/nunito/400.css";
-import "@fontsource/nunito/500.css";
-import "@fontsource/nunito/700.css";
 import { Badge, Button, createTheme, Tooltip } from "@mantine/core";
 import badgeClasses from "./Badge.module.css";
 import buttonClasses from "./Button.module.css";
 import tooltipClasses from "./Tooltip.module.css";
 
 export const theme = createTheme({
-  fontFamily: "Nunito, sans-serif",
-  headings: { fontFamily: "Nunito, sans-serif" },
+  fontFamily: "LanaPixel, sans-serif",
+  headings: { fontFamily: "LanaPixel, sans-serif" },
   components: {
     Badge: Badge.extend({
       classNames: {
